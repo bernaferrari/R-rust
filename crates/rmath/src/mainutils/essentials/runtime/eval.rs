f60c6100f82c58c933f6cb0ee7a1aef4dff83921
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-09-12 23:18:51 -0300

    Differentiate cos, sqrt, and tan in GNU D
    -sin(x), 0.5*x^-0.5, 1/cos(x)^2. Case 643. Gate 706/706.

--- crates/rmath/src/mainutils/essentials/runtime/eval.rs
+++ crates/rmath/src/mainutils/essentials/runtime/eval.rs
@@ -415,6 +415,44 @@ unsafe fn d_diff(expr: SEXP, var: &str) -> SEXP {
                 );
             }
         }
+        if name == "cos" {
+            let arg = CAR(CDR(expr));
+            if d_symbol_name(arg) == var {
+                let s = crate::sexp::constructors::Rf_lang2(Rf_install(c"sin".as_ptr()), arg);
+                return crate::sexp::constructors::Rf_lang2(Rf_install(c"-".as_ptr()), s);
+            }
+        }
+        if name == "sqrt" {
+            let arg = CAR(CDR(expr));
+            if d_symbol_name(arg) == var {
+                let pow = crate::sexp::constructors::Rf_lang3(
+                    Rf_install(c"^".as_ptr()),
+                    arg,
+                    Rf_ScalarReal(-0.5),
+                );
+                return crate::sexp::constructors::Rf_lang3(
+                    Rf_install(c"*".as_ptr()),
+                    Rf_ScalarReal(0.5),
+                    pow,
+                );
+            }
+        }
+        if name == "tan" {
+            let arg = CAR(CDR(expr));
+            if d_symbol_name(arg) == var {
+                let c = crate::sexp::constructors::Rf_lang2(Rf_install(c"cos".as_ptr()), arg);
+                let c2 = crate::sexp::constructors::Rf_lang3(
+                    Rf_install(c"^".as_ptr()),
+                    c,
+                    Rf_ScalarReal(2.0),
+                );
+                return crate::sexp::constructors::Rf_lang3(
+                    Rf_install(c"/".as_ptr()),
+                    Rf_ScalarReal(1.0),
+                    c2,
+                );
+            }
+        }
         Rf_ScalarInteger(0)
     }
 }
---
+++ tests/conformance/cases/643_D_trig.R
@@ -0,0 +1,3 @@
+cat(deparse(D(expression(cos(x)), "x")), "\n", sep = "")
+cat(deparse(D(expression(sqrt(x)), "x")), "\n", sep = "")
+cat(deparse(D(expression(tan(x)), "x")), "\n", sep = "")
